fda2431516e33300701425af26b263cca48a4f80
stanleyjustin <justinhstanley@gmail.com>
2020-10-30 14:03:13 -0300

    first commit

---
+++ debian/init-setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#-------------------------#
+# | Add backports/repos
+#-------------------------#
+
+# vs-codium
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/vscodium.gpg
+echo 'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list
+
+# spotify
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+#-------------------------#
+# | Update sources
+#-------------------------#
+
+sudo apt-get -y update
+
+#-------------------------#
+# | Install packages
+#-------------------------#
+
+sudo apt-get -y install \
+	codium \
+	spotify-client \
+	clamav \
+	clamtk \
+	neofetch \
+	keepassxc \
+	libreoffice \
+	filezilla \
+	vlc \
+	chromium \
+	git \
+	flameshot \
+	virt-manager \	
+
+
+
---
+++ git-setup/git-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# | Confirgure git
+#
+
+git config --global user.name "stanleyjustin"
+git config --global user.email "justinhstanley@gmail.com"
+git config --global init.defaultBranch main
+git config --global color.ui auto
+
+# | keygen
+
+ssh-keygen -C justinhstanley@gmail.com
+
+# | open key in vscodium
+
+codium ~/.ssh/id_rsa.pub
