744d441b5c89997b0fa57913fff8c6868a8e7877
Justin Stanley <65904945+stanleyjustin@users.noreply.github.com>
2020-12-13 13:45:46 -0400

    remove spotify repo and install via snap - add chromium

--- ubuntu/init-setup.sh
+++ ubuntu/init-setup.sh
@@ -8,10 +8,6 @@
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/vscodium.gpg
 echo 'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list
 
-# spotify
-curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-
 #-------------------------#
 # | Update sources
 #-------------------------#
@@ -34,7 +30,9 @@ sudo apt-get -y install \
 	vlc \
 	git \
 	curl \
-	flameshot	
+	flameshot \
+	chromium-browser
 
+snap install spotify
 
 
