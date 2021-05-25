237646a720b817a27a979876d9d37e681ded5ec8
Justin Stanley <65904945+stanleyjustin@users.noreply.github.com>
2021-05-25 11:07:43 -0300

    Update install-third-party-applications.sh

--- install-third-party-applications.sh
+++ install-third-party-applications.sh
@@ -14,15 +14,29 @@ echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sou
 # ADD VERACRYPT #
 sudo wget https://launchpad.net/veracrypt/trunk/1.24-update7/+download/veracrypt-1.24-Update7-Ubuntu-20.04-amd64.deb
 
-# Add GOOGLE CHROME #
+# ADD GOOGLE CHROME #
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
+# ADD DOCKER & DOCKER COMPOSE #
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+ "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
 
 # INSTALL PACKAGES #
 sudo apt update && sudo apt install code \
   spotify-client \
+  docker-ce \
+  docker-ce-cli \
+  containerd.io \
   ./veracrypt-1.24-Update7-Ubuntu-20.04-amd64.deb \
   ./google-chrome-stable_current_amd64.deb
-
+  
+# INSTALL DOCKER COMPOSE #
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
 
 
