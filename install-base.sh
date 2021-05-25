97a139578ff802ae6c9ceb0488a80841e5cfe1ba
Justin Stanley <65904945+stanleyjustin@users.noreply.github.com>
2021-05-25 08:20:24 -0300

    Update install-base.sh

--- install-base.sh
+++ install-base.sh
@@ -4,22 +4,16 @@ sudo apt -y update && sudo apt -y upgrade
 
 sudo apt -y install \
 	curl \
-	git	
-	clamav \
-	clamtk \
-	neofetch \
+	git \	
 	keepassxc \
 	libreoffice \
 	filezilla \
 	vlc \
 	flameshot \
-	chromium \
-	gnome-shell-extension-appindicator \
-	firmware-iwlwifi \
 	syncthing \
 	android-sdk-platform-tools-common \
-	mpv \
-	gnome-tweak-tool \
+	gnome-tweaks
+	
 
 
 
